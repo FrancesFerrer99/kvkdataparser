e52fbee1a404a15c4faf72bfc7b1ba8b8178221e
FrancesFerrer <frances.ferrer1999@gmail.com>
2024-05-29 11:30:41 +0200

    added header and layout route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import IndexPage from "../Pages/IndexPage";
 import UserPage from "../Pages/UserPage";
 import axios from "axios";
+import Layout from "../Components/Layout";
 
 axios.defaults.baseURL = 'http://127.0.0.1:4000'
 axios.defaults.withCredentials = true
@@ -9,8 +10,10 @@ axios.defaults.withCredentials = true
 export default function App() {
   return (
     <Routes>
-      <Route index element={<IndexPage />} />
-      <Route path='/user' element={<UserPage />} />
+      <Route path='/' element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path='/user' element={<UserPage />} />
+      </Route>
     </Routes>
   )
 }
