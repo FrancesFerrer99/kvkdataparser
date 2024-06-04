3176b4577f6aa79c640023b9334a66819913f866
FrancesFerrer <frances.ferrer1999@gmail.com>
2024-06-04 13:27:34 +0200

    Minor refactor

--- client/Pages/KDPage.jsx
+++ client/Pages/KDPage.jsx
@@ -45,6 +45,7 @@ export default function KDPage() {
             <select id="topNSelect" onChange={e => handleRangeChange(e)} value={topN}>
                 <option value={10}>Top 10</option>
                 <option value={20}>Top 20</option>
+                <option value={30}>Top 30</option>
                 <option value={50}>Top 50</option>
                 <option value={100}>Top 100</option>
             </select>
